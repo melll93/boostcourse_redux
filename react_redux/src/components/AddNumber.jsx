d9f8604f7e9769b8848070a21d10c2a2ceeed0c8
melll93 <yemeltu@naver.com>
2023-03-16 00:54:28 +0900

    react_redux start

---
+++ react_redux/src/components/AddNumber.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class AddNumber extends Component {
+    state = { size: 0 }
+    render() {
+        return (
+            <div>
+                <h1>Add Number</h1>
+                <input type="button" value="+" onClick={(() => {
+                    this.props.onClick(this.state.size)
+                }).bind(this)} />
+                <input type="text" value={this.state.size} onChange={((e) => {
+                    this.setState({ size: Number(e.target.value) })
+                }).bind(this)} />
+            </div>
+        );
+    }
+}
