61dba5d263c2a4a72f11da85e75e1b3c4f1722d1
melll93 <yemeltu@naver.com>
2023-03-18 14:26:57 +0900

    redux end

--- react_redux/src/containers/AddNumber.jsx
+++ react_redux/src/containers/AddNumber.jsx
@@ -1,11 +1,20 @@
 import AddNumber from "../components/AddNumber";
-import React, { Component } from "react";
-import store from "../store";
+import { connect } from 'react-redux';
 
-export default class extends Component {
-    render() {
-        return <AddNumber onClick={((size) => {
-            store.dispatch({ type: 'INCREMENT', size: size })
-        }).bind(this)} />
+const mapReduxDispatchToReactProps = (dispatch) => {
+    return {
+        onClick: ((size) => {
+            dispatch({ type: 'INCREMENT', size: size })
+        }).bind(this)
     }
 }
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
+
+// import React, { Component } from "react";
+// export default class extends Component {
+//     render() {
+//         return <AddNumber onClick={((size) => {
+//             store.dispatch({ type: 'INCREMENT', size: size })
+//         }).bind(this)} />
+//     }
+// }
--- react_redux/src/containers/DisplayNumber.jsx
+++ react_redux/src/containers/DisplayNumber.jsx
@@ -1,16 +1,23 @@
-import React, { Component } from "react";
 import DisplayNumber from "../components/DisplayNumber";
-import store from "../store";
-
-export default class extends Component {
-    state = { number: store.getState().number }
-    constructor(props) {
-        super(props);
-        store.subscribe((() => {
-            this.setState({ number: store.getState().number })
-        }).bind(this))
-    }
-    render() {
-        return <DisplayNumber number={this.state.number} />
+import { connect } from 'react-redux'
+const mapReduxStateToReactProps = (state) => {
+    return {
+        number: state.number
     }
 }
+
+export default connect(mapReduxStateToReactProps)(DisplayNumber);
+
+// import React, { Component } from "react";
+// export default class extends Component {
+//     state = { number: store.getState().number }
+//     constructor(props) {
+//         super(props);
+//         store.subscribe((() => {
+//             this.setState({ number: store.getState().number })
+//         }).bind(this))
+//     }
+//     render() {
+//         return <DisplayNumber number={this.state.number} />
+//     }
+// }
